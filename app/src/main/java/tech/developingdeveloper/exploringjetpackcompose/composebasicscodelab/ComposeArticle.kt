bf280bb76a75b6927a1abca415d63b9b08cbb8db
Abhishek Saxena <5abhisheksaxena@gmail.com>
2022-05-15 21:21:27 +0530

    Adjusting padding.

--- app/src/main/java/tech/developingdeveloper/exploringjetpackcompose/composebasicscodelab/ComposeArticle.kt
+++ app/src/main/java/tech/developingdeveloper/exploringjetpackcompose/composebasicscodelab/ComposeArticle.kt
@@ -2,7 +2,9 @@ package tech.developingdeveloper.exploringjetpackcompose.composebasicscodelab
 
 import android.content.res.Configuration
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -29,23 +31,27 @@ fun ComposeArticle() {
             modifier = Modifier.fillMaxWidth(),
         )
 
-        Text(
-            text = "Jetpack Compose tutorial",
-            fontSize = 24.sp,
-            modifier = Modifier.padding(16.dp)
-        )
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(all = 16.dp)
+        ) {
+            Text(
+                text = "Jetpack Compose tutorial",
+                fontSize = 24.sp,
+            )
 
-        Text(
-            text = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
-            textAlign = TextAlign.Justify,
-            modifier = Modifier.padding(horizontal = 16.dp)
-        )
+            Text(
+                text = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
+                textAlign = TextAlign.Justify,
+            )
 
-        Text(
-            text = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.",
-            textAlign = TextAlign.Justify,
-            modifier = Modifier.padding(16.dp)
-        )
+            Text(
+                text = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.",
+                textAlign = TextAlign.Justify,
+            )
+        }
     }
 }
 
