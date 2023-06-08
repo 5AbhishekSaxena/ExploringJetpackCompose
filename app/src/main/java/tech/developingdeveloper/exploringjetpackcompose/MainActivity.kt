aa869e7e13d0056ae72a9ccd014ef0bed015070e
Abhishek Saxena <5abhisheksaxena@gmail.com>
2023-06-08 23:06:24 +0530

    Added LemonadeApp in the MainActivity.

--- app/src/main/java/tech/developingdeveloper/exploringjetpackcompose/MainActivity.kt
+++ app/src/main/java/tech/developingdeveloper/exploringjetpackcompose/MainActivity.kt
@@ -1,6 +1,5 @@
 package tech.developingdeveloper.exploringjetpackcompose
 
-import android.app.Application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,10 +7,9 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
-import tech.developingdeveloper.exploringjetpackcompose.composebasicscodelab.unit_one.business_card.BusinessCard
-import tech.developingdeveloper.exploringjetpackcompose.composebasicscodelab.unit_two.DiceApp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import tech.developingdeveloper.exploringjetpackcompose.composebasicscodelab.unit_two.lemonade.LemonAppViewModel
 import tech.developingdeveloper.exploringjetpackcompose.composebasicscodelab.unit_two.lemonade.LemonadeApp
 import tech.developingdeveloper.exploringjetpackcompose.ui.theme.ExploringJetpackComposeTheme
@@ -20,10 +18,21 @@ import tech.developingdeveloper.exploringjetpackcompose.ui.theme.ExploringJetpac
 @ExperimentalComposeUiApi
 @ExperimentalAnimationApi
 class MainActivity : ComponentActivity() {
+    @Suppress("UNCHECKED_CAST")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val factory = object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return LemonAppViewModel(application) as T
+            }
+        }
+
+        val lemonViewModel = ViewModelProvider(this, factory)
+            .get<LemonAppViewModel>()
+
         setContent {
-            ExploringJetpackComposeApp()
+            ExploringJetpackComposeApp(lemonViewModel)
         }
     }
 }
@@ -32,7 +41,7 @@ class MainActivity : ComponentActivity() {
 @ExperimentalComposeUiApi
 @ExperimentalMaterialApi
 @ExperimentalAnimationApi
-private fun ExploringJetpackComposeApp() {
+private fun ExploringJetpackComposeApp(lemonViewModel: LemonAppViewModel) {
     ExploringJetpackComposeTheme {
         // PhilipContent()
 //        HitAndTryContent()
@@ -41,15 +50,7 @@ private fun ExploringJetpackComposeApp() {
 //        ComposeQuadrant()
 //        BusinessCard()
 //        DiceApp()
-        LemonadeApp(viewModel = LemonAppViewModel(LocalContext.current.applicationContext as Application)) // better way is to inject using hilt - hiltViewModel() or using VMProviders
+//      better way is to inject using hilt - hiltViewModel() or using VMProviders
+        LemonadeApp(viewModel = lemonViewModel)
     }
-}
-
-@Composable
-@ExperimentalMaterialApi
-@ExperimentalComposeUiApi
-@ExperimentalAnimationApi
-@Preview(showBackground = true)
-fun DefaultPreview() {
-    ExploringJetpackComposeApp()
 }
